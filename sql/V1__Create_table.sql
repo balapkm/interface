66b4e619634574535ca08da215c66ffa3a67d2f1
balakumaran <balakumaran.raji@gmail.com>
2019-06-23 20:06:19 +0530

    sql

---
+++ sql/V1__Create_table.sql
@@ -0,0 +1,23 @@
+-- Adminer 4.7.1 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `common_iocl_log_details`;
+CREATE TABLE `common_iocl_log_details` (
+  `UID_Number` int(11) NOT NULL AUTO_INCREMENT,
+  `service_id` int(11) NOT NULL,
+  `service_request_id` varchar(100) NOT NULL,
+  `trip_no` bigint(20) NOT NULL,
+  `IOCL_UID_Number` varchar(100) NOT NULL,
+  `return_status` enum('S','F') NOT NULL DEFAULT 'F',
+  `error_message` varchar(250) NOT NULL,
+  `requested_date_time` datetime NOT NULL,
+  `ack_date_time` datetime NOT NULL,
+  PRIMARY KEY (`UID_Number`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2019-06-23 08:11:37
--- sql/V2__Add_data.sql
+++ sql/V2__Add_data.sql
@@ -1,3 +1,2 @@
-
 INSERT INTO `common_iocl_log_details` (`service_id`, `service_request_id`, `trip_no`, `IOCL_UID_Number`, `return_status`, `error_message`, `requested_date_time`, `ack_date_time`)
 VALUES ('1', '1', '101', '8888', 2, '222', now(), now());
